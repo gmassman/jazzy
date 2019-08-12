d17a467b4336521ad07d2ef3caefde09566f3bb7
Garrett Massman <gmassman20@gmail.com>
2019-08-11 20:10:51 -0700

    Added inputs for Song and Composer fields Small refactoring, replaced PDFPage with useEffect for rendering into canvas.

--- jazzy-fe/src/EnlargedPDF.js
+++ jazzy-fe/src/EnlargedPDF.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { FixedSizeList as List } from 'react-window'
 import pdfjs from 'pdfjs-dist/build/pdf'
 
@@ -6,72 +6,106 @@ import config from './config'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
-function renderPDFPage(page) {
-    return page.then(pdfPage => {
-            const viewport = pdfPage.getViewport({scale: config.fullScale})
-    
-            const id = `page-${pdfPage.pageIndex}`
-            const canvas = document.getElementById(id)
-            const context = canvas.getContext('2d')
-    
-            canvas.height = viewport.height
-            canvas.width = viewport.width
+function renderPDFPage(pageLoader, canvas) {
+    return pageLoader.then(pdfPage => {
+        const viewport = pdfPage.getViewport({scale: config.fullScale})
 
-            const renderContext = {
-                canvasContext: context,
-                viewport: viewport
-            };
-            return pdfPage.render(renderContext).promise
-        })
-        .catch(error => {
-            console.log(error)
-        })
+        const context = canvas.getContext('2d')
+
+        canvas.height = viewport.height
+        canvas.width = viewport.width
+
+        const renderContext = {
+            canvasContext: context,
+            viewport: viewport
+        }
+        const renderTask = pdfPage.render(renderContext)
+        return renderTask.promise
+    })
+    .catch(error => {
+        console.log(error)
+    })
 }
 
-function PDFPage({page}) {
-    renderPDFPage(page)
-        .then(() => {
-            console.log(`rendered page ${page}`)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    return (
-        <>
-        </>
-    )
-}
+const Input = ({ value, onChangeInput, children }) => (
+    <label>
+        {children}
+        <input type="text" value={value} onChange={onChangeInput} />
+    </label>
+)
 
-const MemoizedPDFPage = React.memo(PDFPage)
-
-function EnlargedPDF(item) {
-    const [pagesLoaded, setPagesLoaded] = useState(false)
-    const [PDFPages, setPDFPages] = useState([])
-    const [dimensions, setDimensions] = useState(null)
+function EnlargedPDF(activePDF) {
+    const [state, setState] = useState({
+        pagesLoaded: false,
+        pageLoaders: [],
+        dimensions: null
+    })
+    const [currentPDF, setCurrentPDF] = useState(activePDF)
 
     useEffect(() => {
-        if (PDFPages.length > 0) {
+        if (state.pageLoaders.length > 0) {
             return
         }
-        const pdfData = atob(item.contents)
+        const pdfData = atob(activePDF.contents)
         pdfjs.getDocument({data: pdfData}).promise
             .then(pdf => {
                 const pageLoaders = Array.from({length: pdf.numPages}, (_, i) => pdf.getPage(i+1))
-                setPDFPages(pageLoaders)
-                getDimensions(pageLoaders[0], pdf.numPages).then(results => {
-                    setDimensions(results)
-                    setPagesLoaded(true)
+                getDimensions(pageLoaders[0], pdf.numPages).then(dimensions => {
+                    setState({
+                        pagesLoaded: true,
+                        pageLoaders: pageLoaders,
+                        dimensions: dimensions
+                    })
                 })
             })
     })
 
+    const handleSongChange = event => {
+        event.preventDefault()
+        setCurrentPDF({
+            ...currentPDF,
+            song: event.target.value
+        })
+    }
+
+    const handleComposerChange = event => {
+        event.preventDefault()
+        setCurrentPDF({
+            ...currentPDF,
+            composer: event.target.value
+        })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        // do axios post to update the record
+    }
+
     const Column = ({ index, style }) => {
-        const pdfPage = PDFPages[index]
+        const canvasRef = useRef(null)
+
+        useEffect(() => {
+            const canvas = canvasRef.current
+
+            if (!canvas) return
+
+            renderPDFPage(state.pageLoaders[index], canvas)
+                .then(() => {
+                    console.log(`rendered page into canvas#${canvas.id}`)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        })
+
         style = { ...style, display: 'flex' }
+
         return (
             <div key={`pdf-page-${index}`} style={style}>
-                <canvas id={`page-${index}`}></canvas>
-                <MemoizedPDFPage page={pdfPage}></MemoizedPDFPage>
+                <canvas
+                    ref={canvasRef}
+                    id={`page-${index}`}
+                />
             </div>
         )
     }
@@ -88,15 +122,23 @@ function EnlargedPDF(item) {
         })
     }
 
-    return pagesLoaded ? (
-        <List
-            layout='horizontal'
-            itemCount={PDFPages.length}
-            itemSize={Math.ceil(dimensions.width / PDFPages.length)}
-            {...dimensions}
-        >
-            {Column}
-        </List>
+    return state.pagesLoaded ? (
+        <>
+            <Input value={currentPDF.song} onChangeInput={handleSongChange}>
+                Song:
+            </Input>
+            <Input value={currentPDF.composer} onChangeInput={handleComposerChange}>
+                Composer:
+            </Input>
+            <List
+                layout='horizontal'
+                itemCount={state.pageLoaders.length}
+                itemSize={Math.ceil(state.dimensions.width / state.pageLoaders.length)}
+                {...state.dimensions}
+            >
+                {Column}
+            </List>
+        </>
     ) : (
         <span>loading...</span>
     )
